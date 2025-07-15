1255917f0a2149e118a5866d569a96cf8e46107a
Amit maity <amitmaity2024@gmail.com>
2025-07-16 01:04:51 +0530

    create api get all user list

---
+++ controllers/userMasterController.js
@@ -0,0 +1,39 @@
+const sql = require("mssql");
+const PDO = require("../core/pod.js");
+
+
+exports.getAllUserList = async (params) => {
+  const {
+    id,
+    PageNo,
+    PageSize,
+    Search,
+    user_id,
+    company_id,
+  } = params;
+  
+  const pdo = new PDO();
+  const { data, output } = await pdo.callProcedure({
+    procName: "sp_get_list_UserMast",
+    inputParams: [
+      { name: "id", type: sql.Int, value: id },
+      { name: "PageNo", type: sql.Int, value: PageNo },
+      { name: "PageSize", type: sql.Int, value: PageSize },
+      { name: "Search", type: sql.VarChar(200), value: Search },
+      { name: "user_id", type: sql.Int, value: user_id },
+      { name: "company_id", type: sql.Int, value: company_id },
+    ],
+    outputParams: [
+      { name: "StatusID", type: sql.Int },
+      { name: "StatusMessage", type: sql.VarChar(200) },
+      { name: "TotalCount", type: sql.Int },
+    ],
+  });
+
+  return {
+    data: data,
+    StatusID: output.StatusID,
+    StatusMessage: output.StatusMessage,
+    TotalCount: output.TotalCount
+  };
+};
