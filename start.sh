12f810f423f59d95f0451302c814e945d14cfb12
Tuhin <159275423+tuhin-su@users.noreply.github.com>
2025-07-16 07:40:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 if [ ! -d "./node_modules" ]; then
   npm install
 fi
-check PRODUCTION=true
-# if [ "$PRODUCTION" = true ]; then
-#   npm run serve
-# fi
+#check PRODUCTION=true
+if [ "$PRODUCTION" = true ]; then
+   git fetch --all && git pull
+fi
 npm run dev
