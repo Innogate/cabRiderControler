fdbb2fff8d54ecca492a31b5aa721ff76f6d1731
Amit maity <amitmaity2024@gmail.com>
2025-07-18 23:52:59 +0530

    complite add guest api

--- controllers/guestMasterController.js
+++ controllers/guestMasterController.js
@@ -1,7 +1,7 @@
 const sql = require("mssql");
 const PDO = require("../core/pod.js");
 
-exports.gateAllGuest = async (params) => {
+exports.getAllGuest = async (params) => {
   const {
     id = 0,
     PageNo = 1,
@@ -39,4 +39,54 @@ exports.gateAllGuest = async (params) => {
     TotalCount: output.TotalCount
   };
 };
+
+exports.createGuest = async (params) => {
+  const {
+    id,
+    PartyID,
+    GuestName,
+    AddrType,
+    Addrr,
+    ContactNo,
+    WhatsappNo,
+    Email_ID,
+    Honorific,
+    user_id,
+    company_id,
+  } = params;
+  const pdo = new PDO();
+  const { data, output } = await pdo.callProcedure({
+    procName: "sp_app_create_Guest",
+    inputParams: [
+      { name: "id", type: sql.Int, value: id },
+      { name: "PartyID", type: sql.Int, value: PartyID },
+      { name: "GuestName", type: sql.NVarChar(500), value: GuestName },
+      { name: "AddrType", type: sql.NVarChar(500), value: AddrType },
+      { name: "Addrr", type: sql.NVarChar(500), value: Addrr },
+      { name: "ContactNo", type: sql.NVarChar(50), value: ContactNo },
+      { name: "WhatsappNo", type: sql.NVarChar(50), value: WhatsappNo },
+      { name: "Email_ID", type: sql.NVarChar(50), value: Email_ID },
+      { name: "Honorific", type: sql.NVarChar(50), value: Honorific },
+      { name: "user_id", type: sql.Int, value: user_id },
+      { name: "company_id", type: sql.Int, value: company_id },
+    ],
+    outputParams: [
+      { name: "StatusID", type: sql.Int },
+      { name: "StatusMessage", type: sql.VarChar(200) },
+      { name: "TotalCount", type: sql.Int },
+    ],
+  });
+
+  return {
+    data: data,
+    StatusID: output.StatusID,
+    StatusMessage: output.StatusMessage,
+    TotalCount: output.TotalCount
+  };
+};
+
+
+exports.updateGuest = async (params) =>{
+  
+} 
 
