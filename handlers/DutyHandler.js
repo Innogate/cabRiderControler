8c3924a4b9ca29def6c861166547f3912e16dc46
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-08-01 21:24:09 +0530

    Fix booking service

--- handlers/DutyHandler.js
+++ handlers/DutyHandler.js
@@ -2,34 +2,35 @@ const WebSocketHandler = require("../core/WebSocketHandler");
 const jwt = require("../core/jwt");
 //controllers
 const {
-    appAllotmentBooking,
+  appAllotmentBooking,
 } = require("../controllers/dutyController");
 
 class DutyHandler extends WebSocketHandler {
-    constructor(){
-        super();
-        this.publicCommands = [];
-    }
+  constructor() {
+    super();
+    this.publicCommands = [];
+  }
 
-    async allotBooking(){
-        this.requireAuth();
-        const params = {
-            ...this.body,
-            user_id: this._user.Id,
-            company_id: this._user.company_id,
-        };
-        const result = await appAllotmentBooking(params);
+  async allotBooking() {
+    this.requireAuth();
+    const params = {
+      ...this.body,
+      user_id: this._user.Id,
+      company_id: this._user.company_id,
+    };
+    const result = await appAllotmentBooking(params);
 
-        if (result.message !== 'success') {
+    if (result.status == null) {
       this.send({
         type: "warn",
-        msg: result.message,
+        msg: "Booking allotment failed!",
         ...result,
       });
     } else {
       this.send({
         for: "bookingAllotted",
         msg: "Booking successfully allotted!",
+        type: "success",
         ...result,
       });
     }
